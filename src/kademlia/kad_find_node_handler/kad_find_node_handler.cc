59fb88dd9a5d4ff9715a6b4121acda63114a3e54
Jannik Luhn <jannik.luhn@brainbot.com>
2018-09-19 12:59:36 +0200

    Implement find node handler

---
+++ src/kademlia/kad_find_node_handler/kad_find_node_handler.cc
@@ -0,0 +1,43 @@
+#include <omnetpp.h>
+#include "../kademlia_packets_m.h"
+#include "kad_find_node_handler.h"
+
+using namespace omnetpp;
+
+
+Define_Module(KadFindNodeHandler);
+
+
+void KadFindNodeHandler::initialize() {
+    const char *peer_table_path = par("peerTablePath").stringValue();
+    peer_table = check_and_cast<KademliaPeerTable *>(getModuleByPath(peer_table_path));
+}
+
+void KadFindNodeHandler::handleMessage(cMessage *msg) {
+    KadFindNode *find_node = check_and_cast<KadFindNode *>(msg);
+    int sender = find_node->getSender();
+    int shard = find_node->getShard();
+    KadId sender_kad_id = {sender, shard};
+    int target_node = find_node->getTargetNode();
+    int target_shard = find_node->getTargetShard();
+    KadId target_kad_id = {target_node, target_shard};
+
+    peer_table->updateIfKnown(sender_kad_id);
+
+    std::vector<KadId> neighbors = peer_table->getNeighbors(target_kad_id);
+    EV_DEBUG << "received FindNode for " << target_shard << "/" << target_node
+        << ", replying with " << neighbors.size() << " neighbors" << endl;
+
+    KadNeighbors *reply = new KadNeighbors();
+    reply->setShard(par("shardId"));
+    reply->setNodesArraySize(neighbors.size());
+    reply->setShardsArraySize(neighbors.size());
+    for (int i = 0; i < neighbors.size(); i++) {
+        reply->setNodes(i, neighbors[i].node_id);
+        reply->setShards(i, neighbors[i].shard_id);
+    }
+    reply->setReceiver(sender);
+    send(reply, "out");
+
+    delete find_node;
+}
---
+++ src/kademlia/kad_find_node_handler/kad_find_node_handler.h
@@ -0,0 +1,20 @@
+#ifndef KADEMLIA_KAD_FIND_NODE_HANDLER_KAD_FIND_NODE_HANDLER_H_
+#define KADEMLIA_KAD_FIND_NODE_HANDLER_KAD_FIND_NODE_HANDLER_H_
+
+#include <omnetpp.h>
+#include "../kademlia_peer_table/kademlia_peer_table.h"
+
+using namespace omnetpp;
+
+
+class KadFindNodeHandler : public cSimpleModule {
+  private:
+    KademliaPeerTable *peer_table;
+
+  protected:
+    void initialize();
+    void handleMessage(cMessage *msg);
+};
+
+
+#endif  // KADEMLIA_KAD_FIND_NODE_HANDLER_KAD_FIND_NODE_HANDLER_H_
